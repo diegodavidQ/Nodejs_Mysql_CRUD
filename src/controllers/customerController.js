27331d48d58e97e519bc499033e9e72e156c7039
Diego David Q <diegodavidq@outlook.com>
2021-08-13 12:38:01 -0500

    update customer

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -19,11 +19,44 @@ controller.save = (req, res) => {
 
     req.getConnection((err, conn) => {
         conn.query('INSERT INTO customer set ?', [data], (err, customer) =>{
-            console.log(customer);
-            res.send('works');
-        })
+            res.redirect('/');
+        });
     })
+};
+
+controller.edit = (req, res) => {
+    const { id } = req.params;
+
+    req.getConnection((err, conn) => {
+        conn.query('SELECT * FROM customer WHERE id = ?', [id], (err, customer) => {
+            res.render('customer_edit', {
+                data: customer[0]
+            });
+        });
+    });
 }
+
+controller.update = (req, res) => {
+    const { id } = req.params;
+    const newCustomer = req.body;
+    req.getConnection((err, conn) =>{
+        conn.query('UPDATE customer set ? WHERE id = ?', [newCustomer, id], (err, rows) => {
+            res.redirect('/');
+        });
+    });
+
+}
+
+controller.delete = (req, res) => {
+    const { id } = req.params;
+
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM customer WHERE id = ?', [id], (err, rows) =>{
+            res.redirect('/');
+        });
+    })
+};
+
 
 module.exports = controller;
 
